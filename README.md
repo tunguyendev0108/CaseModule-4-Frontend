9677f135b17f9413b3490cbe83b10e3fc2e7d456
DESKTOP-L93Q7FQ\Admin <nguyenhoangtu1898@gmail.com>
2024-03-22 10:29:20 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# t2d-shop-client-app
---
+++ assets/js/login.js
@@ -0,0 +1,31 @@
+function login() {
+  // let loginDto = {
+  //   username: $("input[name='username']").val(),
+  //   password: $("input[name='password']").val(),
+  // };
+  let requestBody = {  username: $("input[name='username']").val(),
+  password: $("input[name='password']").val(), };
+  console.log(requestBody);
+  // console.log(requestBody);
+  // console.log(typeof requestBody);
+
+  $.ajax({
+    url: `http://localhost:8080/api/auth/login`,
+    type: "POST",
+      dataType: "json",
+    contentType: "application/json",
+    data: JSON.stringify(requestBody),
+    success: function (response, status) {
+      localStorage.setItem("accessToken", response.token);
+      console.log(response.data);
+      let result = JSON.stringify(response);
+      console.log(result);
+      alert("Login Successfully!");
+      window.location.href = "/index-2.html";
+    },
+    error(errorThrown) {
+      console.log(errorThrown);
+    },
+  });
+  
+}
